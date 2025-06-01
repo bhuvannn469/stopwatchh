2cf2a2505276a50f11871c956e5153d841d1efe7
Bhuvan A <120236015+bhuvannn469@users.noreply.github.com>
2025-06-01 20:36:46 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Stopwatch from './Stopwatch';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My Stopwatch</h1>
+      <Stopwatch />
+    </div>
+  );
+}
+
+export default App;
